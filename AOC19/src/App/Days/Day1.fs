d9fdbe0c2f78ac578e002d0bf88398417762f5e5
KotaReid <reid161@live.com>
2021-09-18 12:40:47 -0400

    Day 1 Complete

--- AOC19/src/App/Days/Day1.fs
+++ AOC19/src/App/Days/Day1.fs
@@ -1,10 +1,28 @@
 module Day1
 
-open System
 open System.IO
 
-let readLines filePath = File.ReadLines(filePath)
+let getModules (filePath: string) = 
+    File.ReadLines(filePath) 
+    |> Seq.map int
+
+let requiredFuel mass = mass / 3 - 2
+
+let requiredFuel2 (mass: int) =
+    let rec reqFuel mass =
+        if mass <= 0 then 0
+        else mass + (reqFuel (requiredFuel mass))
+    requiredFuel mass |> reqFuel
+
+let part1 input = 
+    Seq.sumBy requiredFuel input
+    |> printfn "Part 1 %i"
+
+let part2 input =
+    Seq.sumBy requiredFuel2 input
+    |> printfn "Part 2 %i"
 
 let run =
-    let lines = readLines "Files/day1.txt"
-    lines |> Seq.iter (fun t -> Console.WriteLine(t))
+    let input = getModules "Files/day1.txt" 
+    part1 input
+    part2 input
