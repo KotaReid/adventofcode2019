e538d1eee69b960d4ddccafe36f9be4aef514d39
KotaReid <reid161@live.com>
2021-10-03 15:45:50 -0400

    refactor: move computer functionality to type/class

--- AOC19/src/App/Days/Day2.fs
+++ AOC19/src/App/Days/Day2.fs
@@ -2,57 +2,66 @@ module Day2
 
 open System.IO
 
+(*
+let i = [|1L;0L;0L;0L;99L|]
+let c = new Computer(i);;
+*)
+
+type ComputerRunResult =
+    | Success
+    | InvalidOp of int64
+
+type Computer(instructions: int64 array) = 
+    let memory = Array.copy instructions
+
+    let readMemory pos = memory.[(int)memory.[pos]] 
+    
+    let setMemory pos value = memory.[(int)memory.[pos]] <- value
+
+    let fourInstructionOp pos (op: int64 -> int64 -> int64) =
+        let readPos1 = readMemory (pos + 1) 
+        let readPos2 = readMemory (pos + 2)
+        setMemory (pos + 3) (op readPos1 readPos2)
+        pos + 4
+
+    let add pos = fourInstructionOp pos (+)
+
+    let multiply pos = fourInstructionOp pos ( * )
+
+    let rec run pos = 
+        let instruction = memory.[pos]
+        match instruction with
+        | 1L -> add pos |> run 
+        | 2L -> multiply pos |> run
+        | 99L -> Success
+        | _ -> InvalidOp instruction
+
+    member this.Run() = run 0
+
+    member this.Set pos value = memory.[pos] <- value
+
+    member this.Get pos = memory.[pos] 
+
 let getInstructions (filePath: string) =
     let codes = File.ReadLines(filePath) |> Seq.head
     codes.Split ',' |> Array.map int64
 
-type ComputerResult =
-    | Success of int64
-    | InvalidOp of int64
-
-let readMemory (instructions: int64 array) (pos: int64) =
-    instructions.[(int) pos]
-
-let setMemory (instructions: int64 array) (pos: int64) (value: int64) =
-    instructions.[(int) pos] <- value
-
-let fourInstructionOp (instructions: int64 array) (pos: int64) (op: int64 -> int64 -> int64) =
-    let readPos1 = readMemory instructions (pos + 1L) |> readMemory instructions
-    let readPos2 = readMemory instructions (pos + 2L) |> readMemory instructions
-    let setPostion = readMemory instructions (pos + 3L)
-    setMemory instructions setPostion (op readPos1 readPos2)
-    pos + 4L
-
-//Opcode 1
-let add (instructions: int64 array) (pos: int64) = 
-    fourInstructionOp instructions pos (+)
-
-//Opcode 2
-let multiply (instructions: int64 array) (pos: int64) =
-    fourInstructionOp instructions pos (*)
-
-let rec computer (instructions: int64 array) (answerPos: int64) (pos: int64) =
-    let instruction = readMemory instructions pos
-    match instruction with
-    | 1L -> add instructions pos |> computer instructions answerPos
-    | 2L -> multiply instructions pos |> computer instructions answerPos
-    | 99L -> readMemory instructions answerPos |> Success
-    | _ -> InvalidOp instruction
-
 let part1 (instructions: int64 array) = 
-    instructions.[1] <- 12L
-    instructions.[2] <- 2L 
-    match computer instructions 0L 0L with 
-    | Success result -> printfn "Part 1 %i" result
+    let computer = Computer instructions
+    computer.Set 1 12L
+    computer.Set 2 2L
+    
+    match computer.Run() with 
+    | Success -> computer.Get 0 |> printfn "Part 1 %i"
     | InvalidOp error -> printfn "Part 1 encountered an invalid opcode %i" error
 
 let rec part2 (instructions: int64 array) = 
     let solve noun verb = 
-        let instructionsCopy = Array.copy instructions
-        instructionsCopy.[1] <- noun
-        instructionsCopy.[2] <- verb
-        match computer instructionsCopy 0L 0L with
-        | Success result -> result
+        let computer = Computer instructions
+        computer.Set 1 noun
+        computer.Set 2 verb
+        match computer.Run() with
+        | Success -> computer.Get 0
         | InvalidOp e -> -1L
     
     seq {
@@ -63,5 +72,5 @@ let rec part2 (instructions: int64 array) =
 
 let run() =
     let instructions = getInstructions "Files/day2.txt"
-    Array.copy instructions |> part1
-    Array.copy instructions |> part2
+    part1 instructions
+    part2 instructions
